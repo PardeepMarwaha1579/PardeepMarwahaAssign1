8c51ccf18c00da9f6c0c5b9181908286be0552f8
PardeepMarwaha1579 <84354559+PardeepMarwaha1579@users.noreply.github.com>
2021-06-06 09:23:18 +0530

    Refined scanner code to throw error

--- src/pardeep/marwaha/s991631579/CardTrick.java
+++ src/pardeep/marwaha/s991631579/CardTrick.java
@@ -29,10 +29,34 @@ public class CardTrick {
        Scanner scan=new Scanner(System.in);
        System.out.println("Please Select your card suit:");
        System.out.println("Please chose:"+"\n"+"Type 0 for Hearts"+"\n"+"Type 1 for Diamonds"+"\n"+"Type 2 for Spades"+"\n"+"Type 3 for Clubs");
-       int suitValue=scan.nextInt();
+       int suitValue=0;
+       try
+       {
+           suitValue=scan.nextInt();
+           if(suitValue<0||suitValue>3){
+               throw new Exception();
+           }
+       }
+       catch(Exception e)
+       {
+                    System.out.println("Please enter suit Number from 0-3 only!");
+                    suitValue=scan.nextInt();
+       }
        
        System.out.println("Type the Card Value from 1 to 13"+"\n"+"i.e Ace = 1, Jack =11, Queen =12, King = 13");
-       int cardNumber=scan.nextInt();
+       int cardNumber=0;
+       try
+       {
+           cardNumber=scan.nextInt();
+           if(cardNumber<0||cardNumber>3){
+               throw new Exception();
+           }
+       }
+       catch(Exception e)
+       {
+                    System.out.println("Please enter suit Number from 1-13 only!");
+                    cardNumber=scan.nextInt();
+       }
        System.out.println("Your Card is:"+Card.SUITS[suitValue]+" "+cardNumber); 
        
             
