673c184460ebc10338f03e5152fe12560f52b900
PardeepMarwaha1579 <84354559+PardeepMarwaha1579@users.noreply.github.com>
2021-06-06 08:59:54 +0530

    Added code for taking suit value input from user and printing the user's card

--- src/pardeep/marwaha/s991631579/CardTrick.java
+++ src/pardeep/marwaha/s991631579/CardTrick.java
@@ -30,12 +30,15 @@ public class CardTrick {
        System.out.println("Please Select your card suit:");
        System.out.println("Please chose:"+"\n"+"Type 0 for Hearts"+"\n"+"Type 1 for Diamonds"+"\n"+"Type 2 for Spades"+"\n"+"Type 3 for Clubs");
        int suitValue=scan.nextInt();
-            //c.setValue(insert call to random number generator here)
-            //c.setSuit(Card.SUITS[insert call to random number between 0-3 here])
+       
+       System.out.println("Type the Card Value from 1 to 13"+"\n"+"i.e Ace = 1, Jack =11, Queen =12, King = 13");
+       int cardNumber=scan.nextInt();
+       System.out.println("Your Card is:"+Card.SUITS[suitValue]+" "+cardNumber); 
+       
+            
+       
         }
 
-        //insert code to ask the user for Card value and suit, create their card
-        // and search magicHand here
-        //Then report the result here
+        
     }
         
